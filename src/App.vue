1e36b3d4f5c0365e49e0c0335bb3fda741c89ea2
augustaspocius <nesuspameris@gmail.com>
2023-04-10 19:01:55 +0300

    add mobile menu, cart flyout, styling changes on mobile

--- src/App.vue
+++ src/App.vue
@@ -1,96 +1,157 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import Main from './components/Main.vue'
+import MobileMenu from './components/MobileMenu.vue'
+import CartFlyout from './components/CartFlyout.vue';
 
 export default defineComponent({
   name: 'App',
+  components: {
+    Main,
+    MobileMenu,
+    CartFlyout
+  },
+  data(){
+    return {
+      mobileMenuOpen: false,
+      quantity: 0,
+      cartOpen: false,
+    }
+  },
+  methods: {
+    increment(){
+      this.quantity++
+    },
+    decrement(){
+      if(this.quantity > 0){
+        this.quantity--
+      }
+    }
+  },
 });
 </script>
 
 <template>
-  <div class="font-kumbh-sans bg-neutral-light-grayish-blue min-h-screen">
-    <header class="bg-primary-orange py-4 px-6 flex justify-between items-center">
-      <!--Nav bar-->
-      <nav>
-        <ul class="flex space-x-4 text-white font-bold">
-          <li>Collections</li>
-          <li>Men</li>
-          <li>Women</li>
-          <li>About</li>
-          <li>Contact</li>
-        </ul>
-      </nav>
-
-      <!--Logo-->
-      <div class="text-white font-bold text-2xl">
-        Logo
+  <div class="font-kumbh-sans min-h-screen">
+    <MobileMenu :isOpen="mobileMenuOpen" @close="mobileMenuOpen = false" />
+    <header class="py-5 px-6 flex justify-between items-center">
+      <!-- Hamburger menu -->
+      <div class="flex justify-between items-center gap-4">
+        <div class="md:hidden">
+          <button class="flex justify-center items-center" @click="mobileMenuOpen = !mobileMenuOpen">
+            <!-- Add hamburger menu icon here -->
+            <img src="./assets/icon-menu.svg" alt="menu" />
+          </button>
+        </div>
+        <!-- Logo -->
+        <img class="self-center pb-1" src="./assets/logo.svg" alt="sneakers"/>
       </div>
 
-      <!--Icons-->
-      <div class="flex space-x-4 text-white">
-        <!--Cart icon-->
-        <i class="fas fa-shopping-cart"></i>
-        <!--User icon-->
-        <i class="fas fa-user"></i>
+      <!-- Icons -->
+      <div class="flex gap-5">
+        <!-- Cart icon -->
+        <button @click="cartOpen = !cartOpen">
+            <img src="./assets/icon-cart.svg" alt="cart" />
+        </button>
+        <!-- Profile icon -->
+        <button class="p-1 rounded-full ring-2 ring-transparent hover:ring-primary-orange">
+          <img class="max-w-[1.5rem]" src="./assets/image-avatar.png" alt="avatar" />
+        </button>
       </div>
     </header>
 
-    <main class="container mx-auto px-4 py-6">
+    <main class="container mx-auto">
       <!-- Main content -->
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-12">
+      <div class="grid grid-cols-1">
         <!-- Product image gallery -->
-        <div>
+        <div class="relative w-full">
           <!-- Add your image gallery component here -->
+          <img src="./assets/image-product-1.jpg" alt="product" />
+          <!-- Left button -->
+          <button class="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white rounded-full w-10 h-10 flex justify-center items-center">
+            <!-- Add left arrow icon here -->
+            <img class="w-3 h-auto" src="./assets/icon-previous.svg" alt="icon previous">
+          </button>
+          <!-- Right button -->
+          <button class="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white rounded-full w-10 h-10 flex justify-center items-center">
+            <!-- Add right arrow icon here -->
+            <img class="w-3 h-auto" src="./assets/icon-next.svg" alt="icon next">
+          </button>
+          
+          <div class="absolute left-0 right-0 top-0">
+            <CartFlyout :isOpen="cartOpen" />
+          </div>
         </div>
 
         <!-- Product details -->
-        <div>
+        <div class="px-6  pt-6">
           <!-- Brand above the product name -->
-          <h3 class="text-neutral-dark-grayish-blue font-bold text-xl mb-2">
+          <h3 class="text-primary-orange font-bold text-xs mb-2 uppercase tracking-[1.85px]">
             Sneaker Company
           </h3>
 
           <!-- Product name -->
-          <h1 class="text-neutral-very-dark-blue font-bold text-3xl mb-4">
+          <h1 class="text-neutral-very-dark-blue font-bold text-[28px] leading-8 mb-4">
             Fall Limited Edition Sneakers
           </h1>
 
           <!-- Product description -->
-          <p class="text-neutral-dark-grayish-blue mb-6">
+          <p class="text-neutral-dark-grayish-blue text-[15px] mb-6">
             These low-profile sneakers are your perfect casual wear companion.
             Featuring a durable rubber outer sole, they’ll withstand everything the
             weather can offer.
           </p>
 
           <!-- Pricing -->
-          <div class="flex items-baseline mb-4">
+          <div class="flex items-center mb-4 justify-between">
             <!-- Discounted price -->
-            <span class="text-primary-orange text-2xl font-bold mr-4">
-              $125.00
-            </span>
-            <!-- Discount -->
-            <span class="text-neutral-dark-grayish-blue font-bold mr-2">
-              50%
-            </span>
+            <div class="flex items-center justify-between">
+              <span class="text-neutral-very-dark-blue text-[28px] leading-9 font-bold mr-4">
+                $125.00
+              </span>
+              <!-- Discount -->
+              <span class="bg-primary-pale-orange text-primary-orange font-bold py-0.5 px-2 rounded-md">
+                50%
+              </span>
+            </div>
             <!-- Original price -->
-            <span class="text-neutral-grayish-blue line-through">
+            <span class="text-neutral-grayish-blue font-bold line-through">
               $250.00
             </span>
           </div>
 
           <!-- Quantity and add to cart button -->
-          <div class="flex items-center">
+          <div class="mb-4">
             <!-- Quantity -->
-            <input
-              type="number"
-              min="0"
-              class="border-2 border-neutral-dark-grayish-blue w-20 text-center mr-4"
-            />
-            <!-- Add to cart button -->
-            <button class="bg-primary-orange text-white font-bold py-2 px-4 rounded">
-              Add to cart
-            </button>
+            <div class="flex justify-between ound items-center bg-neutral-greyish-white border-0 rounded-xl
+             py-[22px] px-[24px]">
+              <button @click="decrement"
+                      class="text-primary-orange px-3">
+                <!-- Add minus icon here -->
+                <img src="./assets/icon-minus.svg" alt="icon minus">
+              </button>
+              <input
+                type="number"
+                class="w-full text-center bg-transparent font-bold"
+                v-model="quantity"
+                min="0"
+              />
+              <button
+                @click="increment"
+                class="text-primary-orange px-3 py-1">
+              <!-- Add plus icon here -->
+              
+                <img src="./assets/icon-plus.svg" alt="icon plus">
+              </button>
+            </div>
           </div>
+
+          <!-- Add to cart button -->
+          <button class="w-full bg-primary-orange text-white font-bold py-[19px] rounded-xl flex justify-center items-center gap-1 shadow-btn">
+            <!-- Add cart icon here -->
+            <img class="w-4 filter sepia-0 saturate-0 brightness-200 contrast-200 hue-rotate-15" src="./assets/icon-cart.svg" alt="cart" />
+            <span class="ml-2">Add to cart</span>
+          </button>
         </div>
       </div>
     </main>
---
+++ src/components/CartFlyout.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+    export default {
+        props: {
+            isOpen: {
+            type: Boolean,
+            default: false,
+            },
+        },
+    };
+</script>
+<template>
+    <div
+      class="bg-white w-[95%] rounded-lg shadow-md flex flex-col m-auto mt-2"
+      :class="{ 'hidden': !isOpen }"
+    >
+      <h2 class="font-bold text-lg mb-4 p-4">Cart</h2>
+      <div class="bg-gray-300 h-[1px] mb-4 w-full"></div>
+      <ul class="p-4">
+        <li class="flex items-center justify-between mb-4">
+          <img class="w-12 h-12 mr-2" src="../assets/image-product-1-thumbnail.jpg" alt="product" />
+          <div>
+            <span class="text-neutral-dark-grayish-blue text-[15px]">Fall Limited Edition Sneakers</span>
+            <span class="text-neutral-dark-grayish-blue text-[15px]">$125.00 x 1</span>
+            <p class="font-bold">$125.00</p>
+          </div>
+          <button>
+            <img src="../assets/icon-delete.svg" alt="delete" />
+          </button>
+        </li>
+        <!-- Add more list items for each product in the cart -->
+      </ul>
+      <div class="p-4">
+          <button class="w-full py-5 text-base bg-primary-orange text-white font-bold rounded-xl">
+            Checkout
+          </button>
+      </div>
+    </div>
+</template>
+
+  
