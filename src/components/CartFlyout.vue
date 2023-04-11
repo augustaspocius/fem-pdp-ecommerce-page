fe6676df18dfdebfa0dfcd54c53c73eeceab2912
augustaspocius <nesuspameris@gmail.com>
2023-04-11 14:08:41 +0300

    cart flyout mobile layout changes

--- src/components/CartFlyout.vue
+++ src/components/CartFlyout.vue
@@ -13,17 +13,20 @@
       class="bg-white w-[95%] rounded-lg shadow-md flex flex-col m-auto mt-2"
       :class="{ 'hidden': !isOpen }"
     >
-      <h2 class="font-bold text-lg mb-4 p-4">Cart</h2>
-      <div class="bg-gray-300 h-[1px] mb-4 w-full"></div>
-      <ul class="p-4">
-        <li class="flex items-center justify-between mb-4">
-          <img class="w-12 h-12 mr-2" src="../assets/image-product-1-thumbnail.jpg" alt="product" />
+      <h2 class="font-bold text-lg mb-2 p-4">Cart</h2>
+      <div class="bg-gray-300 h-[1px] mb-6 w-full"></div>
+      <ul class="px-4 pb-4">
+        <li class="flex items-center justify-between">
+          <img class="w-[50px] h-[50px] rounded" src="../assets/image-product-1-thumbnail.jpg" alt="product" />
           <div>
             <span class="text-neutral-dark-grayish-blue text-[15px]">Fall Limited Edition Sneakers</span>
-            <span class="text-neutral-dark-grayish-blue text-[15px]">$125.00 x 1</span>
-            <p class="font-bold">$125.00</p>
+            <br>
+            <div class="flex gap-1 items-center justify-start">
+                <span class="text-neutral-dark-grayish-blue text-[15px]">$125.00 x 3</span>
+                <p class="inline font-bold">$375.00</p>
+            </div>
           </div>
-          <button>
+          <button class="pl-5">
             <img src="../assets/icon-delete.svg" alt="delete" />
           </button>
         </li>
