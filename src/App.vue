0a085eb6e5f6947a162baabe10076b5a5858bf6c
augustaspocius <nesuspameris@gmail.com>
2023-04-12 08:47:52 +0300

    add to cart implementation

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,14 @@ import { defineComponent } from 'vue';
 import Main from './components/Main.vue'
 import MobileMenu from './components/MobileMenu.vue'
 import CartFlyout from './components/CartFlyout.vue';
+import { CartItem } from './types';
+
+interface DataProps {
+  mobileMenuOpen: boolean;
+  quantity: number;
+  cartOpen: boolean;
+  cartItems: CartItem[];
+}
 
 export default defineComponent({
   name: 'App',
@@ -11,22 +19,51 @@ export default defineComponent({
     MobileMenu,
     CartFlyout
   },
-  data(){
+  data(): DataProps {
     return {
       mobileMenuOpen: false,
       quantity: 0,
       cartOpen: false,
+      cartItems: []
     }
   },
+  computed: {
+    totalQuantity(): number {
+      return this.cartItems.reduce((acc: number, item: CartItem) => acc + item.quantity, 0);
+    },
+  },
   methods: {
-    increment(){
+    increment() {
       this.quantity++
     },
-    decrement(){
-      if(this.quantity > 0){
+    decrement() {
+      if (this.quantity > 0) {
         this.quantity--
       }
-    }
+    },
+    addToCart() {
+      const newItem = {
+        id: 1, // You can assign a unique ID for each product
+        name: 'Fall Limited Edition Sneakers',
+        price: 125,
+        originalPrice: 250,
+        discount: 50,
+        quantity: this.quantity,
+      };
+
+      const existingItemIndex = this.cartItems.findIndex(
+        (item) => item.id === newItem.id
+      );
+
+      if (existingItemIndex !== -1) {
+        this.cartItems[existingItemIndex].quantity += newItem.quantity;
+      } else {
+        this.cartItems.push(newItem);
+      }
+    },
+    handleRemoveFromCart(itemId: number) {
+      this.cartItems = this.cartItems.filter((item: CartItem) => item.id !== itemId);
+    },
   },
 });
 </script>
@@ -44,14 +81,18 @@ export default defineComponent({
           </button>
         </div>
         <!-- Logo -->
-        <img class="self-center pb-1" src="./assets/logo.svg" alt="sneakers"/>
+        <img class="self-center pb-1" src="./assets/logo.svg" alt="sneakers" />
       </div>
 
       <!-- Icons -->
       <div class="flex gap-5">
         <!-- Cart icon -->
-        <button @click="cartOpen = !cartOpen">
-            <img src="./assets/icon-cart.svg" alt="cart" />
+        <button class="relative" @click="cartOpen = !cartOpen">
+          <img src="./assets/icon-cart.svg" alt="cart" />
+          <span v-if="totalQuantity > 0"
+            class="absolute top-[-3px] right-[-5px] text-white text-[10px] font-bold bg-primary-orange rounded-full w-5 flex items-center justify-center">
+            {{ totalQuantity }}
+          </span>
         </button>
         <!-- Profile icon -->
         <button class="p-1 rounded-full ring-2 ring-transparent hover:ring-primary-orange">
@@ -68,18 +109,20 @@ export default defineComponent({
           <!-- Add your image gallery component here -->
           <img src="./assets/image-product-1.jpg" alt="product" />
           <!-- Left button -->
-          <button class="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white rounded-full w-10 h-10 flex justify-center items-center">
+          <button
+            class="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white rounded-full w-10 h-10 flex justify-center items-center">
             <!-- Add left arrow icon here -->
             <img class="w-3 h-auto" src="./assets/icon-previous.svg" alt="icon previous">
           </button>
           <!-- Right button -->
-          <button class="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white rounded-full w-10 h-10 flex justify-center items-center">
+          <button
+            class="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white rounded-full w-10 h-10 flex justify-center items-center">
             <!-- Add right arrow icon here -->
             <img class="w-3 h-auto" src="./assets/icon-next.svg" alt="icon next">
           </button>
-          
+
           <div class="absolute left-0 right-0 top-0">
-            <CartFlyout :isOpen="cartOpen" />
+            <CartFlyout :isOpen="cartOpen" :cartItems="cartItems" @remove-item="handleRemoveFromCart" />
           </div>
         </div>
 
@@ -124,34 +167,29 @@ export default defineComponent({
           <div class="mb-4">
             <!-- Quantity -->
             <div class="flex justify-between ound items-center bg-neutral-greyish-white border-0 rounded-xl
-             py-[22px] px-[24px]">
-              <button @click="decrement"
-                      class="text-primary-orange px-3">
+               py-[22px] px-[24px]">
+              <button @click="decrement" class="text-primary-orange px-3">
                 <!-- Add minus icon here -->
                 <img src="./assets/icon-minus.svg" alt="icon minus">
               </button>
-              <input
-                type="number"
-                class="w-full text-center bg-transparent font-bold"
-                v-model="quantity"
-                min="0"
-              />
-              <button
-                @click="increment"
-                class="text-primary-orange px-3 py-1">
-              <!-- Add plus icon here -->
-              
+              <input type="number" class="w-full text-center bg-transparent font-bold" v-model="quantity" min="0" />
+              <button @click="increment" class="text-primary-orange px-3 py-1">
+                <!-- Add plus icon here -->
+
                 <img src="./assets/icon-plus.svg" alt="icon plus">
               </button>
             </div>
           </div>
 
           <!-- Add to cart button -->
-          <button class="w-full bg-primary-orange text-white font-bold py-[19px] rounded-xl flex justify-center items-center gap-1 shadow-btn">
+          <button @click="addToCart"
+            class="w-full bg-primary-orange text-white font-bold py-[19px] rounded-xl flex justify-center items-center gap-1 button-shadow">
             <!-- Add cart icon here -->
-            <img class="w-4 filter sepia-0 saturate-0 brightness-200 contrast-200 hue-rotate-15" src="./assets/icon-cart.svg" alt="cart" />
+            <img class="w-4 filter sepia-0 saturate-0 brightness-200 contrast-200 hue-rotate-15"
+              src="./assets/icon-cart.svg" alt="cart" />
             <span class="ml-2">Add to cart</span>
           </button>
+
         </div>
       </div>
     </main>
---
+++ src/types.ts
@@ -0,0 +1,9 @@
+export interface CartItem {
+    id: number;
+    name: string;
+    price: number;
+    originalPrice: number;
+    discount: number;
+    quantity: number;
+  }
+  
