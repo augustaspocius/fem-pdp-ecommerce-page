e88106a1eb4f484217e04fbb2b1f34e0d32f773c
augustaspocius <nesuspameris@gmail.com>
2023-04-16 21:04:06 +0300

    removing the comments

--- src/App.vue
+++ src/App.vue
@@ -85,9 +85,7 @@ export default defineComponent({
     />
 
     <main class="container mx-auto">
-      <!-- Main content -->
       <div class="grid grid-cols-1">
-        <!-- Product image gallery -->
         <div class="relative w-full">
           <ImageSlider />
           <div class="absolute left-0 right-0 top-0">
@@ -95,65 +93,50 @@ export default defineComponent({
           </div>
         </div>
 
-        <!-- Product details -->
         <div class="px-6  pt-6">
-          <!-- Brand above the product name -->
           <h3 class="text-primary-orange font-bold text-xs mb-2 uppercase tracking-[1.85px]">
             Sneaker Company
           </h3>
 
-          <!-- Product name -->
           <h1 class="text-neutral-very-dark-blue font-bold text-[28px] leading-8 mb-4">
             Fall Limited Edition Sneakers
           </h1>
 
-          <!-- Product description -->
           <p class="text-neutral-dark-grayish-blue text-[15px] mb-6">
             These low-profile sneakers are your perfect casual wear companion.
             Featuring a durable rubber outer sole, they’ll withstand everything the
             weather can offer.
           </p>
 
-          <!-- Pricing -->
           <div class="flex items-center mb-4 justify-between">
-            <!-- Discounted price -->
             <div class="flex items-center justify-between">
               <span class="text-neutral-very-dark-blue text-[28px] leading-9 font-bold mr-4">
                 $125.00
               </span>
-              <!-- Discount -->
               <span class="bg-primary-pale-orange text-primary-orange font-bold py-0.5 px-2 rounded-md">
                 50%
               </span>
             </div>
-            <!-- Original price -->
             <span class="text-neutral-grayish-blue font-bold line-through">
               $250.00
             </span>
           </div>
 
-          <!-- Quantity and add to cart button -->
           <div class="mb-4">
-            <!-- Quantity -->
             <div class="flex justify-between ound items-center bg-neutral-greyish-white border-0 rounded-xl
                py-[22px] px-[24px]">
               <button @click="decrement" class="text-primary-orange px-3">
-                <!-- Add minus icon here -->
                 <img src="./assets/icon-minus.svg" alt="icon minus">
               </button>
               <input type="number" class="w-full text-center bg-transparent font-bold" v-model="quantity" min="0" />
               <button @click="increment" class="text-primary-orange px-3 py-1">
-                <!-- Add plus icon here -->
-
                 <img src="./assets/icon-plus.svg" alt="icon plus">
               </button>
             </div>
           </div>
 
-          <!-- Add to cart button -->
           <button @click="addToCart"
             class="w-full bg-primary-orange text-white font-bold py-[19px] rounded-xl flex justify-center items-center gap-1 button-shadow">
-            <!-- Add cart icon here -->
             <img class="w-4 filter sepia-0 saturate-0 brightness-200 contrast-200 hue-rotate-15"
               src="./assets/icon-cart.svg" alt="cart" />
             <span class="ml-2">Add to cart</span>
