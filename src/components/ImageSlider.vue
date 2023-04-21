1ee95b52643a1c40fa3247ed4ff4d78d825f04af
augustaspocius <nesuspameris@gmail.com>
2023-04-21 16:41:31 +0300

    pdp gallery changes

--- src/components/ImageSlider.vue
+++ src/components/ImageSlider.vue
@@ -33,52 +33,55 @@ export default {
         this.currentSlide = this.images.length - 1;
       }
     },
+    selectSlide(index: number): void {
+      this.currentSlide = index;
+    },
   },
 };
 </script>
 <template>
-    <div class="relative w-full overflow-x-hidden">
-      <div
-        class="w-full whitespace-nowrap transition-all duration-500"
-        :style="slideStyles"
-      >
-        <img
-          v-for="(image, index) in images"
-          :key="index"
-          class="w-full h-full inline-block 
-                md:rounded-xl"
-          :src="image"
-          alt="product"
-        />
-      </div>
-  
-      <!-- Left button -->
-      <button
-        class="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white rounded-full w-10 h-10 flex justify-center items-center
-               md:hidden"
-        @click="previousSlide"
-      >
-        <img src="/src/assets/icon-previous.svg" alt="icon previous" />
-      </button>
-      <!-- Right button -->
-      <button
-        class="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white rounded-full w-10 h-10 flex justify-center items-center
-               md:hidden"
-        @click="nextSlide"
-      >
-        <img src="/src/assets/icon-next.svg" alt="icon next" />
-      </button>
-    </div>
-    
+  <div class="relative w-full overflow-x-hidden">
     <div
-        class="hidden w-full md:flex pt-8 gap-8"
-      >
-        <img
-          v-for="(image, index) in images"
-          :key="index"
-          class="w-full max-w-[88px] rounded-xl border-2 border-transparent hover:border-primary-orange hover:cursor-pointer hover:opacity-50"
-          :src="image"
-          alt="product"
-        />
-      </div>
-  </template>
+      class="w-full whitespace-nowrap transition-all duration-500"
+      :style="slideStyles"
+    >
+      <img
+        v-for="(image, index) in images"
+        :key="index"
+        class="w-full h-full inline-block 
+              md:rounded-xl"
+        :src="image"
+        alt="product"
+      />
+    </div>
+
+    <!-- Left button -->
+    <button
+      class="absolute top-1/2 left-4 transform -translate-y-1/2 bg-white rounded-full w-10 h-10 flex justify-center items-center
+             md:hidden"
+      @click="previousSlide"
+    >
+      <img src="/src/assets/icon-previous.svg" alt="icon previous" />
+    </button>
+    <!-- Right button -->
+    <button
+      class="absolute top-1/2 right-4 transform -translate-y-1/2 bg-white rounded-full w-10 h-10 flex justify-center items-center
+             md:hidden"
+      @click="nextSlide"
+    >
+      <img src="/src/assets/icon-next.svg" alt="icon next" />
+    </button>
+  </div>
+
+  <div class="hidden w-full md:flex pt-8 gap-8">
+    <img
+      v-for="(image, index) in images"
+      :key="index"
+      class="w-full max-w-[88px] rounded-xl border-2 border-transparent hover:cursor-pointer hover:opacity-50"
+      :class="{ 'border-primary-orange opacity-50': currentSlide === index }"
+      :src="image"
+      alt="product"
+      @click="selectSlide(index)"
+    />
+  </div>
+</template>
